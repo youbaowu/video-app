afb6a56f294a5bf86842180c673271bad9147221
youbaowu <youbaowu@tencent.com>
2021-04-15 09:18:40 +0800

    Decoding first video frame and rendering to window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
-#include <stdio.h>
 #include <GLFW/glfw3.h>
+#include <stdio.h>
+
+bool load_frame(const char* filename, int* width, int* height, unsigned char** data);
 
 int main(int argc, const char** argv) {
     GLFWwindow* window;
@@ -14,24 +16,15 @@ int main(int argc, const char** argv) {
         printf("Couldn't open window\n");
         return -1;
     }
-
-    unsigned char* data = new unsigned char[100 * 100 * 3];
-    for (int y = 0; y < 100; y++) {
-        for (int x = 0; x < 100; x++) {
-            data[y * 100 * 3 + x * 3] = 0xff;
-            data[y * 100 * 3 + x * 3 + 1] = 0x00;
-            data[y * 100 * 3 + x * 3 + 2] = 0x00;
-        }
-    }
-
-    for (int y = 25; y < 75; y++) {
-        for (int x = 25; x < 75; x++) {
-            data[y * 100 * 3 + x * 3] = 0x00;
-            data[y * 100 * 3 + x * 3 + 1] = 0x00;
-            data[y * 100 * 3 + x * 3 + 2] = 0xff;
-        }
-    }
     
+    int frame_width, frame_height;
+    unsigned char* frame_data;
+    if (!load_frame("/Users/youbaowu/Desktop/1080x1920.mp4", &frame_width, &frame_height, &frame_data)) {
+        printf("Couldn't load video frame\n");
+        return -1;
+    }
+
+
     glfwMakeContextCurrent(window);
     
     GLuint tex_handle;
@@ -43,7 +36,7 @@ int main(int argc, const char** argv) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 100, 100, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, frame_width, frame_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, frame_data);
 
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -53,17 +46,17 @@ int main(int argc, const char** argv) {
         glfwGetFramebufferSize(window, &window_width, &window_height);
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
-        glOrtho(0, window_width, 0, window_height, -1, 1);
+        glOrtho(0, window_width, window_height, 0, -1, 1);
         glMatrixMode(GL_MODELVIEW);
         
         // Render here
         glEnable(GL_TEXTURE_2D);
         glBindTexture(GL_TEXTURE_2D, tex_handle);
         glBegin(GL_QUADS);
-            glTexCoord2d(0, 0); glVertex2i(0, 0);
-            glTexCoord2d(1, 0); glVertex2i(100, 0);
-            glTexCoord2d(1, 1); glVertex2i(100, 100);
-            glTexCoord2d(0, 1); glVertex2i(0, 100);
+            glTexCoord2d(0, 0); glVertex2i(200, 200);
+            glTexCoord2d(1, 0); glVertex2i(200 + frame_width, 200);
+            glTexCoord2d(1, 1); glVertex2i(200 + frame_width, 200 + frame_height);
+            glTexCoord2d(0, 1); glVertex2i(200, 200 + frame_height);
         glEnd();
         glDisable(GL_QUADS);
         
